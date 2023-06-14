5edba4b3fe9b209f645f85cf06c73c2b1e5540da
isotop786 <marufislam77@gmail.com>
2023-06-14 13:39:40 +0600

    page not found added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './public/components/login/login.component';
 import { RegisterComponent } from './public/components/register/register.component';
 import { DashboardComponent } from './secure/components/dashboard/dashboard.component';
 import { OrderComponent } from './secure/components/order/order.component';
+import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component';
 
 const routes: Routes = [
   {
@@ -28,7 +29,8 @@ const routes: Routes = [
   {
     path:'register',
     component:RegisterComponent
-  }
+  },
+  { path: '**', component: PageNotFoundComponentComponent }, 
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SecureModule } from './secure/secure.module';
+import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PageNotFoundComponentComponent,
   ],
   imports: [
     BrowserModule,
